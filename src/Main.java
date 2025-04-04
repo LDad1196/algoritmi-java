2da306a0ff6614a4f3efe2b8ac6ad0e39d58e31c
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2025-04-04 13:33:33 +0200

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        algoritmo9();
+    }
+
+    public static void algoritmo9() {
+        //Dato vettore V=[1,2,3,4,5,6,7] determina se ogni valore dell'array corrisponde al valore del suo indice
+
+    }
+}
