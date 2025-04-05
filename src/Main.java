2a85ac3faf8f5348dcef6b3b6a4f39d84e4945a6
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2025-04-05 11:52:05 +0200

    Added Algoritmo until 19

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        algoritmo17();
+        algoritmo19();
     }
 
-    public static void algoritmo17() {
-        //Inverti il vettore V=[1,2,3,10,5,6,8]
-        int[] V = {1,2,3,10,5,6,8};
-        int n = V.length;
-        int i = 0;
-        while (i < n / 2) {
-            int j = n - 1 - i;
-            int temp = V[i];
-            V[i] = V[j];
-            V[j] = temp;
-            i++;
+    public static void algoritmo19() {
+        int[] v1 = {1,2,3,1,1,2,4,3,5,6,5,7};
+        int n = v1.length;
+        int[] v2 = new int[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            boolean duplicato = false;
+            for (int j = 0; j < count; j++) {
+                if (v1[i] == v2[j]) {
+                    duplicato = true;
+                    break;
+                }
+            }
+            if (!duplicato) {
+                v2[count] = v1[i];
+                System.out.println("In posizione " + count + " del vettore v2 si trova il numero " + v1[i]);
+                count++;
+            }
         }
-        for (i = 0; i < V.length; i++) {
-            System.out.println("il numero nel vettore V in posizione " + i + " ora è " + V[i]);
+        System.out.print("Vettore senza duplicati: ");
+        for (int i = 0; i < count; i++) {
+            System.out.print(v2[i] + " ");
         }
     }
 
