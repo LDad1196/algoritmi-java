471dad6e20c0eb925b37155766e613dbbf822d49
lorenzodaddazio <ldaddazio11@gmail.com>
2025-04-08 13:01:01 +0200

    Aggiunte fatte da mac

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        algoritmo19();
+        algoritmo23();
     }
 
-    public static void algoritmo19() {
-        int[] v1 = {1,2,3,1,1,2,4,3,5,6,5,7};
-        int n = v1.length;
-        int[] v2 = new int[n];
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            boolean duplicato = false;
-            for (int j = 0; j < count; j++) {
-                if (v1[i] == v2[j]) {
-                    duplicato = true;
-                    break;
+    public static void algoritmo23() {
+        //Dato un array di interi ed una target sum, trova gli indice dei 2 numeri che sommati risultano essere il target; diagramma di flusso, java, passo passo, pseudo
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Quanto vuoi che sia lungo il vettore");
+        int n = scan.nextInt();
+        int[] V = new int[n];
+        int i = 0;
+        while (i < n) {
+            System.out.println("Che numero vuoi aggiungere nel vettore in posizione " + i + "?");
+            V[i] = scan.nextInt();
+            i++;
+        }
+        i = 0;
+        System.out.println("Qual'è la somma di numeri che stai cercando?");
+        int targetsum = scan.nextInt();
+        boolean trovata = false;
+        int[] V2 = new int[2];
+        while (i < n - 1 && !trovata) {
+            int j = i + 1;
+            while (j < n && !trovata) {
+                int somma = V[i] + V[j];
+                if (somma == targetsum) {
+                    trovata = true;
+                    V2[0] = i;
+                    V2[1] = j;
                 }
+                j++;
             }
-            if (!duplicato) {
-                v2[count] = v1[i];
-                System.out.println("In posizione " + count + " del vettore v2 si trova il numero " + v1[i]);
-                count++;
-            }
+            i++;
         }
-        System.out.print("Vettore senza duplicati: ");
-        for (int i = 0; i < count; i++) {
-            System.out.print(v2[i] + " ");
+        if (trovata) {
+            System.out.println("La somma " + targetsum + " è stata trovata ed e composta dal numero in posizione " + V2[0] + " e " + V2[1] + " del vettore V, perchè " + V[V2[0]] + "+" + V[V2[1]] + "=" + targetsum);
+        } else {
+            System.out.println("La somma " + targetsum + " non è stata trovata nel vettore V");
         }
     }
-
 }
